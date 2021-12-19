d601a8413c850aa059947ec19b5b3124ac3235c5
Guillermo Bereche <bere0058@algonquinlive.com>
2021-12-18 20:01:24 -0500

    o

--- fuctionalities.js
+++ fuctionalities.js
@@ -2,8 +2,7 @@ const logo = document.getElementById('logo');
 const button = document.getElementById('nav');
 
 button.addEventListener('click', () => {
-    nav.style.height= "100vh";
     nav.classList.remove('navbar');
-    nav.classList.add('width');
-    logo.classList.remove('w-25');
+    nav.classList.toggle('width');
+    logo.classList.toggle('w-25');
 }) ;
